6e1109131606f3b6dceb7178ad8e4ae33d7652b5
Bastien Hoffstetter <bastien.hoffstetter@free.fr>
2018-01-24 17:51:53 +0100

    Q4 et 5

--- tp2.js
+++ tp2.js
@@ -1,32 +1,42 @@
-var disp = 0;
-var intervalId;
+var startCarre = 1;
+var intervalId1, intervalId2;
 
 startFunction = function () {
-    clearInterval(intervalId);
-    intervalId = setInterval(intervalFunction, 10);
+    clearInterval(intervalId1);
+    intervalId1 = setInterval(intervalFunction, 10);
+    intervalId2 = setInterval(startCarreFunc, 1000);
+};
+
+startCarreFunc = function () {
+    startCarre++;
+    if (startCarre === document.getElementsByClassName("carre").length) {
+        clearInterval(intervalId2);
+    }
 };
 
 intervalFunction = function () {
     var carres = document.getElementsByClassName("carre");
 
-    disp += 10;
-
-    for (var i = 0; i < carres.length; i++) {
-        carres[i].style.left = disp + "px";
+    for (var i = 0; i < startCarre; i++) {
+        carres[i].style.left = (parseInt(carres[i].style.left.split("px")[0]) + 10) + "px";
     }
 };
 
-function load() {
+function load(nbCarre) {
     var node = document.getElementById("canvas");
 
-    var redDiv = document.createElement("div");
-    redDiv.className = "carre";
-    redDiv.style.left = "0px";
-
-    node.appendChild(redDiv);
+    for (var i = 0; i < nbCarre; i++) {
+        var redDiv = document.createElement("div");
+        redDiv.className = "carre";
+        redDiv.style.left = "10px";
+        redDiv.style.top = (i*110 + 10) + "px";
+        node.appendChild(redDiv);
+    }
 
     //TP 2
-    // intervalId = setInterval(intervalFunction, 10);
+    // intervalId1 = setInterval(intervalFunction, 10);
     //TP 3
-    intervalId = setInterval(startFunction, 2000);
+    // intervalId1 = setInterval(startFunction, 2000);
+    //TP 4
+    intervalId1 = setInterval(startFunction, 4000);
 }
